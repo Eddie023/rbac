418db20b082d48193f063a990629f9141567ad65
Manish Chaulagain <manish23chaulagain@gmail.com>
2024-10-21 09:49:57 +1100

    chore: rename to database.

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { DBConfig } from './service';
+import { DBConfig } from './database.service';
 
 @Module({
 	imports: [
